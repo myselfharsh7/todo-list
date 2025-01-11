68caa230c5adea42f35b1718373d5c796f9697ce
harsh <harsh@harsh>
2025-01-12 01:22:55 +0530

    staticfiles

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w9_g@qw_yjd9@1p)l)72zb1ynf4s-)!vz)wg+qi!9tt7p)2(1u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-list-svnt.onrender.com','127.0.0.1']
 
 
 # Application definition
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tasks/static'),
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
